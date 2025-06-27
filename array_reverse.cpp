dab82ea6c244c510447eb266babdf99ae5ed66d1
Ayush <ayushvats528@gmail.com>
2025-06-27 21:33:58 +0530

    commited

---
+++ array_reverse.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+void takeInput(int a[], int size) {
+    for (int i = 0; i < size; i++) {
+        cin >> a[i];
+    }
+}
+void swap_array(int a[],int size){
+for (int i = 0; i < size / 2; i++) {
+    swap(a[i], a[size - 1 - i]); // modifying the size inside the loop,
+}                               //which is bad practice and unnecessary.
+                                
+}
+int main(){
+int size;
+cout<<"Enter size of array:";
+cin>>size;
+int a[size];
+takeInput(a, size);
+for (int i = 0; i < size; i++)
+{
+  cout<<a[i]<<" ";
+}
+cout<<endl;
+cout<<"Swapped array:"<<endl;
+swap_array(a, size);
+for (int i = 0; i < size; i++)
+{
+  cout<<a[i]<<" ";
+}
+return 0;
+}
