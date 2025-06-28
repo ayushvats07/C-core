cf3227f3eb824d556f09fed063017c8af0712d0f
Ayush <ayushvats528@gmail.com>
2025-06-28 13:02:53 +0530

    commited

--- vector_functions.cpp
+++ vector_functions.cpp
@@ -1,33 +1,49 @@
-//vector fuctions
+// Vector functions demonstration
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-vector<int>vec;
-//1.size
-cout<<"Intial size:"<<vec.size()<<endl;
-for(int value:vec){
-cout<<value<<" ";
-}
 
-//2.Push_back "to push element"
-vec.push_back(25);
-vec.push_back(35);
-vec.push_back(45);
-vec.push_back(55);
-cout<<"After pushing elements size of vector="<<vec.size()<<endl;
-for(int value:vec){
-cout<<value<<" ";
-}
-cout<<endl;
-//3.Pop_back "to pop last element"
-vec.pop_back();
-cout<<"After poping element size of vecor"<<vec.size()<<endl;
-for(int value:vec){
-cout<<value<<" ";
-}
+int main() {
+    vector<int> vec;
 
+    // 1. size() - shows initial size
+    cout << "Initial size: " << vec.size() << endl;
+    for (int value : vec) {
+        cout << value << " ";
+    }
+    cout << endl;
 
-return 0;
+    // 2. push_back() - to push elements into the vector
+    vec.push_back(25);
+    vec.push_back(35);
+    vec.push_back(55);
+    vec.push_back(75);
+
+    cout << "After pushing elements, size of vector = " << vec.size() << endl;
+    for (int value : vec) {
+        cout << value << " ";
+    }
+    cout << endl;
+
+    // 3. pop_back() - to remove the last element
+    vec.pop_back();
+
+    cout << "After popping an element, size of vector = " << vec.size() << endl;
+    for (int value : vec) {
+        cout << value << " ";
+    }
+    cout << endl;
+
+    // 4. back() - prints the last element
+    cout << "Last element (back): " << vec.back() << endl;
+
+    // 5. front() - prints the first element
+    cout << "First element (front): " << vec.front() << endl;
+
+    // 6. at() and [] - accessing element at index 2
+    cout << "Element at index 2 using at(): " << vec.at(2) << endl;
+    cout << "Element at index 2 using []: " << vec[2] << endl;
+
+    return 0;
 }
