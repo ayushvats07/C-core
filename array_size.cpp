68d164f6bf27355e9ddbd259aba8218dcb3640ce
Ayush <ayushvats528@gmail.com>
2025-06-28 08:51:17 +0530

    commited

---
+++ array_size.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a[5]={1,5,6,9,8};
+cout<<"Size of array is "<<sizeof(a)/4;
+
+
+  return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int>vec={1,4,2,5,6};
+cout<<vec[3];
+
+return 0;
+}
